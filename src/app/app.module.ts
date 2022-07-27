7cd02c90b9e3335dd2e5496235d870179e5aa586
Geeyas <geeyasa@gmail.com>
2022-07-28 02:12:38 +0800

    First upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServicesComponent } from './services/services.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ContactComponent } from './contact/contact.component';
+import { OnlineOrderComponent } from './online-order/online-order.component';
+import { GetQuoteComponent } from './get-quote/get-quote.component';
+
+const routes: Routes = [
+
+  { path: "home", component: HomepageComponent },
+  { path: "about", component: AboutComponent },
+  { path: "services", component: ServicesComponent },
+  { path: "pricing", component: PricingComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "onlineOrder", component: OnlineOrderComponent },
+  { path: "quote", component: GetQuoteComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent,
+    AboutComponent,
+    FooterComponent,
+    ServicesComponent,
+    PricingComponent,
+    OnlineOrderComponent,
+    GetQuoteComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
