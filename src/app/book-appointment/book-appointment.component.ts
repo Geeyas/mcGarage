742e7d4f17c48d620c8d7447c4776749baa7a89b
Geeyas <geeyasa@gmail.com>
2022-08-09 23:32:24 +0800

    book appointment updated

--- src/app/book-appointment/book-appointment.component.ts
+++ src/app/book-appointment/book-appointment.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-book-appointment',
   templateUrl: './book-appointment.component.html',
   styleUrls: ['./book-appointment.component.css']
 })
-export class BookAppointmentComponent implements OnInit {
+export class BookAppointmentComponent {
 
+  name: string = "";
+  number: string = "";
+  email: string = "";
+  appointment: string = ""
+  date: string = "";
+
+  errMsgName: string = "";
+  errMsgNumber: string = "";
+  errMsgApp: string = "";
+  errMsgEmail: string = "";
+  errMsgDate: string = "";
+  successMsg: string = "";
   constructor() { }
 
-  ngOnInit(): void {
+  validate() {
+    if (!this.name) {
+      this.errMsgName = "Full name required";
+      return;
+    } else if (!this.number) {
+      this.errMsgNumber = "Contact number required";
+      return;
+    } else if (isNaN(parseInt(this.number))) {
+      this.errMsgNumber = "Contact number is not valid";
+      return;
+    } else if (!this.email) {
+      this.errMsgEmail = "Email address required";
+      return;
+    } else if (this.appointment == "") {
+      this.errMsgApp = "Select Appointment for";
+      return;
+    } else if (!this.date) {
+      this.errMsgDate = "Select Date";
+      return;
+    } else {
+      this.successMsg = "Appointment Booked succsfully";
+      this.name = "";
+      this.number = "";
+      this.email = "";
+      this.appointment = ""
+      this.date = "";
+    }
+  }
+
+  clear() {
+    this.errMsgName = "";
+    this.errMsgNumber = "";
+    this.errMsgApp = "";
+    this.errMsgEmail = "";
+    this.errMsgDate = "";
   }
 
 }
