75407e8716eab80e16d4be1d419f83ac42a3b8cb
Geeyas <geeyasa@gmail.com>
2022-08-14 18:47:41 +0800

    login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,11 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  username: string = "";
+  message: string = "";
+  password: string = "";
   constructor() { }
 
-  alert() {
-
+  login() {
+    if (!this.username) {
+      this.message = "Email must be entered";
+      return;
+    } else if (!this.password) {
+      this.message = "Password must be entered";
+      return;
+    } else {
+      this.username = "";
+      this.password = "";
+      this.message = "Logged in Successfully";
+    }
   }
 
+  clear() {
+    this.message = "";
+  }
 }
