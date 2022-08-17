c2320b793e9b10c68d18686bf7336f6fe23b7b3a
Geeyas <geeyasa@gmail.com>
2022-08-17 23:21:36 +0800

    carousel api

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -7,6 +7,45 @@ import { Component } from '@angular/core';
 })
 export class HomepageComponent {
 
+  carousel: any = [
+    {
+      "id": 1,
+      "img": "assets/img/pic4.jpg",
+      "alt": "Wild landscape",
+      "text": "Full Car Wrap"
+    },
+    {
+      "id": 2,
+      "img": "assets/img/pic1.jpg",
+      "alt": "Wild landscape",
+      "text": "Full Car Wrap"
+    },
+    {
+      "id": 3,
+      "img": "assets/img/pic3.jpg",
+      "alt": "Wild landscape",
+      "text": "Full Car Wrap"
+    },
+    {
+      "id": 4,
+      "img": "assets/img/pic6.jpg",
+      "alt": "Wild landscape",
+      "text": "Full Car Wrap"
+    },
+    {
+      "id": 5,
+      "img": "assets/img/pic7.jpg",
+      "alt": "Wild landscape",
+      "text": "Full Car Wrap"
+    },
+    {
+      "id": 6,
+      "img": "assets/img/pic8.jpg",
+      "alt": "Wild landscape",
+      "text": "Full Car Wrap"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
